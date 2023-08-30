db4c8c8514d8fd4733eda49d3191dbb225f4ffec
antoniogisondi <anto.gisondi@tiscali.it>
2023-08-30 19:57:05 +0200

    Store and router

---
+++ src/router.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from './pages/HomePage.vue';
+import ProjectList from './pages/ProjectList.vue';
+import ProjectTypeList from './pages/ProjectTypeList.vue';
+import TechnologyList from './pages/TechnologyList.vue';
+import AboutUs from './pages/AboutUs.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'homepage',
+            component: HomePage
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: ProjectList
+        },
+        {
+            path: '/projects-type',
+            name: 'projects_type',
+            component: ProjectTypeList
+        },
+        {
+            path: '/technology',
+            name: 'technology',
+            component: TechnologyList
+        },
+        {
+            path: '/about-us',
+            name: 'about_us',
+            component: AboutUs
+        }
+    ]
+});
+
+export { router };
